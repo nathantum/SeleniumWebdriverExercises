3efb5594fc11e6f7c4595159e2140008c0c2fa04
nathantum <tumkipsang@gmail.com>
2023-03-07 13:58:12 -0600

    finding web elements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private static WebDriver driver;
@@ -11,9 +15,19 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com");
 
-        driver.manage().window().setSize(new Dimension(375, 812));
+        //Exercise:
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText(("Example 1: Menu Element"))).click();
+        List<WebElement> menuItems = driver.findElements(By.tagName("li"));
+        System.out.println("The number of menu items: " + menuItems.size());
 
-        System.out.println(driver.getTitle());
+        //WebElement inputsLinkText =driver.findElement(By.linkText("Inputs"));
+        //inputsLinkText.click();
+
+        //List<WebElement> listOfTags = driver.findElements(By.tagName("a"));
+        //System.out.println(listOfTags.size());
+
+        //System.out.println(driver.getTitle());
         driver.quit();
     }
 
